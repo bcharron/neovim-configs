6e4b93a20a2518727226de23276a4ceb1e06cfd1
Benjamin Charron <bcharron@pobox.com>
2025-11-10 21:32:03 -0500

    Initial commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,7 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	'NMAC427/guess-indent.nvim',
+}
