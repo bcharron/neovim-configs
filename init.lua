ade036db918c00302e1b4f8deb6965f9d7a9edec
Benjamin Charron <bcharron@pobox.com>
2025-11-11 15:22:35 -0500

    Move buffer keys to tabline

--- init.lua
+++ init.lua
@@ -1,31 +1,6 @@
 -- Don't change Yank behavior from vim
 vim.keymap.del('n', 'Y')
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
-
--- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
--- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- map('n', '<C-s-p>', '<Cmd>BufferPickDelete<CR>', opts)
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -201,13 +176,6 @@ require('lazy').setup({
     },
   },
 })
-
--- Must run after which-keys has been loaded
-map('n', '<Leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', { noremap = true, silent = true, desc = 'Order [B]uffers by num[b]er' })
-map('n', '<Leader>bn', '<Cmd>BufferOrderByName<CR>', { noremap = true, silent = true, desc = 'Order [B]uffers by [N]ame' })
-map('n', '<Leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/tabline.lua
+++ lua/custom/plugins/tabline.lua
@@ -13,5 +13,34 @@ return {
     -- insert_at_start = true,
     -- …etc.
   },
+  keys = {
+    { '<Leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', desc = 'Order [B]uffers by num[b]er' },
+    { '<Leader>bn', '<Cmd>BufferOrderByName<CR>', desc = 'Order [B]uffers by [N]ame' },
+    { '<Leader>bd', '<Cmd>BufferOrderByDirectory<CR>', desc = 'Order [B]uffers by [D]irectory' },
+    { '<Leader>bl', '<Cmd>BufferOrderByLanguage<CR>', desc = 'Order [B]uffers by [L]anguage' },
+    { '<Leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', desc = 'Order [B]uffers by [W]indow number' },
+
+    -- Move to previous/next
+    { '<A-,>', '<Cmd>BufferPrevious<CR>' },
+    { '<A-.>', '<Cmd>BufferNext<CR>' },
+
+    -- Re-order to previous/next
+    { '<A-<>', '<Cmd>BufferMovePrevious<CR>' },
+    { '<A->>', '<Cmd>BufferMoveNext<CR>' },
+
+    -- Goto buffer in position...
+    { '<A-1>', '<Cmd>BufferGoto 1<CR>' },
+    { '<A-2>', '<Cmd>BufferGoto 2<CR>' },
+    { '<A-3>', '<Cmd>BufferGoto 3<CR>' },
+    { '<A-4>', '<Cmd>BufferGoto 4<CR>' },
+    { '<A-5>', '<Cmd>BufferGoto 5<CR>' },
+    { '<A-6>', '<Cmd>BufferGoto 6<CR>' },
+    { '<A-7>', '<Cmd>BufferGoto 7<CR>' },
+    { '<A-8>', '<Cmd>BufferGoto 8<CR>' },
+    { '<A-9>', '<Cmd>BufferGoto 9<CR>' },
+    { '<A-0>', '<Cmd>BufferLast<CR>' },
+    { '<A-c>', '<Cmd>BufferClose<CR>' },
+  },
+
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
