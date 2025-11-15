27528dc086402ba88ea486330d3c6d7022d5385b
Benjamin Charron <bcharron@pobox.com>
2025-11-15 01:22:34 -0500

    Stop messing up my clipboard

--- init.lua
+++ init.lua
@@ -18,7 +18,10 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  -- vim.o.clipboard = 'unnamedplus'
+  -- Actually, don't yank/change/delete to clipboard.
+  -- Use "+ to manually access clipboard instead.
+  vim.o.clipboard = 'unnamed'
 end)
 
 -- Enable break indent
